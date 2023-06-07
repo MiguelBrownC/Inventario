54a051204d3df909f158d26911ec0a035c7aedeb
M.Brown <browncoloma@gmailcom>
2023-06-07 13:54:22 -0400

    modificacion devolucion

--- devolucion_update.php
+++ devolucion_update.php
@@ -17,10 +17,10 @@ $estado_dev_col = 1;
 $status_asignacion = 0;
 
 
-//actulizamos el campo de devolucion colaborador 
+//actulizamos el campo de devolucion colaborador esta bien
 $sentencia = $mysqli->prepare("UPDATE colaborador SET
 equipo_devolucioln = ? WHERE id_colaborador = ?");
-$sentencia->bind_param("ii",$estado_dev_col,$col);
+$sentencia->bind_param("ii", $estado_dev_col, $col);
 $sentencia->execute();
 
 
@@ -38,34 +38,35 @@ $total_asignado =  $count;
 
 $sentencia2->free_result();
 
-
+if ($total_asignado > 0) {
   $sentencia3 = $mysqli->prepare("UPDATE colaborador SET
-equipo_colaborador = ? WHERE id_colaborador = ?");
-$sentencia3->bind_param("ii",$status_asignacion ,$col);
-$sentencia3->execute();
+  equipo_colaborador = ? WHERE id_colaborador = ?");
+  $sentencia3->bind_param("ii", $status_asignacion, $col);
+  $sentencia3->execute();
+}
+
 
 
 
 
 // eliminamos el registro de asignacion del equipo
-$query="DELETE FROM asignacion WHERE id_equipo = ?";
+$query = "DELETE FROM asignacion WHERE id_equipo = ?";
 $stmt = $mysqli->prepare($query);
 if ($stmt) {
-$stmt->bind_param('i',$eq);
-$stmt->execute();
-
+  $stmt->bind_param('i', $eq);
+  $stmt->execute();
 }
 
 //actualizamos el campo status_asignacion de equipo para liberar el stock
 $sentencia5 = $mysqli->prepare("UPDATE equipo SET
 status_asignacion = ? WHERE id_equipo = ?");
-$sentencia5->bind_param("ii",$status_asignacion,$eq);
+$sentencia5->bind_param("ii", $status_asignacion, $eq);
 $sentencia5->execute();
 
 // actualizamos la fecha de devolucion equipo en log_asignacion
 $sentencia6 = $mysqli->prepare("UPDATE log_asignacion SET
 fecha_devolucion = ? WHERE id_equipo = ?");
-$sentencia6->bind_param("si",$fecha,$eq);
+$sentencia6->bind_param("si", $fecha, $eq);
 $sentencia6->execute();
 
 ?>
