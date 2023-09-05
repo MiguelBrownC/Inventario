3298b5d4f9f3cc3788be9c507663d1b9738044d2
Miguel Brown C <browncoloma@gmail.com>
2023-09-05 17:28:00 -0300

    prueba sincronizacion

--- generar_reporte_desvinculados.php
+++ generar_reporte_desvinculados.php
@@ -53,11 +53,7 @@ include_once "conexion.php";
         on teq.id_tipo_equipo = e.id_tipo_equipo
         where c.estado_colaborador = 0 and l.fecha_devolucion is not null
         order by c.nombre_colaborador asc;";
-
         $result = $mysqli->query($sql);
-
-
-
 		while ($row_msg_contatos = mysqli_fetch_assoc($result)) {
 			$html .= '<tr>';
 			$html .= '<td>' . $row_msg_contatos["tipo_empresa"] . '</td>';
@@ -70,14 +66,14 @@ include_once "conexion.php";
             $html .= '</tr>';;
 		}
 
-        // Configuración en la cabecera
+        // Configuración en la cabeceras
 		header("Last-Modified: " . gmdate("D,d M Y") . " GMT");
 		header("Cache-Control: no-cache, must-revalidate");
 		header("Pragma: no-cache");
 		header("Content-type: application/x-msexcel");
 		header("Content-Disposition: attachment; filename=\"{$arquivo}\"");
 		header("Content-Description: PHP Generado Data");
-		// Envia contenido al archivo
+		// Envia contenido al archivos
 		echo $html;
 		exit; ?>
     </body>
